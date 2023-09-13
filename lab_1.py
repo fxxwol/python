15c3b3795057d7705eb5da61817b398db4b76fc9
fxxwol <protsivroksolana2004@gmail.com>
2023-09-13 18:16:13 +0300

    added menu and settings menu

--- lab_1.py
+++ lab_1.py
@@ -1,97 +1,81 @@
-# memory = None
-
-# while True:
-#     num1 = float(input("Введіть перше число: "))
-
-#     num2 = float(input("Введіть друге число: "))
-
-#     while True:
-#         operator = input("Введіть оператор (+, -, *, /, ^ для піднесення до степеня, √ для квадратного кореня, % для залишку від ділення): ")
-
-#         if operator not in ['+', '-', '*', '/', '^', '√', '%']:
-#             print("Невірний оператор. Будь ласка, введіть один із +, -, *, /, ^, √, %.")
-#         else:
-#             if operator == '+':
-#                 result = num1 + num2
-#             elif operator == '-':
-#                 result = num1 - num2
-#             elif operator == '*':
-#                 result = num1 * num2
-#             elif operator == '/':
-#                 if num2 == 0:
-#                     print("Помилка: Ділення на нуль.")
-#                 else:
-#                     result = num1 / num2
-#             elif operator == '^':
-#                 result = num1 ** num2
-#             elif operator == '√':
-#                 result = num1 ** 0.5 
-#             elif operator == '%':
-#                 result = num1 % num2
-
-#             print(f"Результат: {num1} {operator} {num2} = {result}")
-            
-#             save_to_memory = input("Зберегти результат в пам'яті? (Так/Ні): ")
-#             if save_to_memory.lower() == 'так':
-#                 memory = result
-#                 break
-
-#     another_calculation = input("Виконати ще одне обчислення? (Так/Ні): ")
-#     if another_calculation.lower() != 'так':
-#         break
-
-# if memory is not None:
-#     print(f"Значення в пам'яті: {memory}")
-
-# Ініціалізуємо список для зберігання історії обчислень
 calculation_history = []
+decimal_places = 2
+
+def settings_menu():
+    while True:
+        print("Меню налаштувань:")
+        print("1. Змінити кількість десяткових розрядів")
+        print("2. Вихід з меню налаштувань")
+        
+        setting_option = input("Виберіть опцію: ")
+        
+        if setting_option == '1':
+            global decimal_places
+            decimal_places = int(input("Введіть нову кількість десяткових розрядів: "))
+        elif setting_option == '2':
+            break
 
 while True:
-    num1 = float(input("Введіть перше число: "))
-    num2 = float(input("Введіть друге число: "))
+    print("Головне меню:")
+    print("1. Викликати меню налаштувань")
+    print("2. Обчислення")
+    print("3. Переглянути історію обчислень")
+    print("4. Вихід")
     
-    while True:
-        operator = input("Введіть оператор (+, -, *, /, ^ для піднесення до степеня, √ для квадратного кореня, % для залишку від ділення): ")
-        
-        if operator not in ['+', '-', '*', '/', '^', '√', '%']:
-            print("Невірний оператор. Будь ласка, введіть один із +, -, *, /, ^, √, %.")
-        else:
-            if operator == '+':
-                result = num1 + num2
-            elif operator == '-':
-                result = num1 - num2
-            elif operator == '*':
-                result = num1 * num2
-            elif operator == '/':
-                if num2 == 0:
-                    print("Помилка: Ділення на нуль.")
+    option = input("Виберіть опцію (1, 2, 3 або 4): ")
+    
+    if option == '1':
+        settings_menu()
+    elif option == '2':
+        while True:
+            num1 = float(input("Введіть перше число: "))
+            num2 = float(input("Введіть друге число: "))
+            
+            while True:
+                while True:
+                    operator = input("Введіть оператор (+, -, *, /, ^ для піднесення до степеня, √ для квадратного кореня, % для залишку від ділення): ")
+                    
+                    if operator not in ['+', '-', '*', '/', '^', '√', '%']:
+                        print("Невірний оператор. Будь ласка, введіть один із +, -, *, /, ^, √, %.")
+                    else:
+                        break
+                
+                if operator == '+':
+                    result = num1 + num2
+                elif operator == '-':
+                    result = num1 - num2
+                elif operator == '*':
+                    result = num1 * num2
+                elif operator == '/':
+                    if num2 == 0:
+                        print("Помилка: Ділення на нуль.")
+                    else:
+                        result = num1 / num2
+                elif operator == '^':
+                    result = num1 ** num2
+                elif operator == '√':
+                    result = num1 ** 0.5 
+                elif operator == '%':
+                    result = num1 % num2
+                
+                calculation_history.append((num1, operator, num2, result))
+                
+                print(f"Результат: {num1} {operator} {num2} = {result:.{decimal_places}f}")
+                
+                another_calculation = input("Виконати ще одне обчислення? (Так/Ні): ")
+                if another_calculation.lower() != 'так':
+                    break
                 else:
-                    result = num1 / num2
-            elif operator == '^':
-                result = num1 ** num2
-            elif operator == '√':
-                result = num1 ** 0.5 
-            elif operator == '%':
-                result = num1 % num2
-
-            # Записуємо обчислення в історію
-            calculation_history.append((num1, operator, num2, result))
-
-            print(f"Результат: {num1} {operator} {num2} = {result}")
+                    num1 = float(input("Введіть перше число: "))
+                    num2 = float(input("Введіть друге число: "))
             break
-    another_calculation = input("Виконати ще одне обчислення? (Так/Ні): ")
-
-    if another_calculation.lower() != 'так':
-        print("Історія обчислень:")
-        for entry in calculation_history:
-            num1, operator, num2, result = entry
-            print(f"{num1} {operator} {num2} = {result}")
+    elif option == '3':
+        if len(calculation_history) != 0:
+            print("Історія обчислень:")
+            for entry in calculation_history:
+                num1, operator, num2, result = entry
+                print(f"{num1} {operator} {num2} = {result:.{decimal_places}f}")
+        else:
+            print("Пам'ять порожня.")
+    elif option == '4':
         break
-# Виводимо результат з пам'яті, якщо він бу
-
-
-
-# Завдання 9: Історія обчислень
-# Створіть журнал, який зберігає історію попередніх обчислень, включаючи вираз і результат. Дозвольте користувачам переглядати історію своїх обчислень.
-# Завдання 10: Налаштування користувача
-# Надайте користувачам можливість налаштувати поведінку калькулятора, таку як зміну кількості десяткових розрядів, які відображаються, або налаштування функцій пам'яті.
